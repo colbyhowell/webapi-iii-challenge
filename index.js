7757e98d8d0171963a34d44ce82af63fb7b5151d
colbyhowell <49887775+colbyhowell@users.noreply.github.com>
2019-11-07 21:02:01 -0600

    comitting before adding to heroku

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-// code away!
+require('dotenv').config()
+const server = require('./server');
 
-const server = require("./server");
+const port = process.env.PORT || 5000
+//  const {PORT} = process.env (case matters when destructured)
 
-server.listen(5000, () => {
-  console.log("___ Server is listening on port 5000___");
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
